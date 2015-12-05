7192030c66686c8c18c1dc6681d829fd6c27736c
Suraj Patil <sapatil@live.com>
2015-12-05 18:15:26 +0530

    localhost if no nw connected

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"archive/zip"
 	"flag"
-	"path/filepath"
 	"io"
 	"log"
 	"net"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -21,9 +21,15 @@ func main() {
 	downloaded = 0
 	addresses, err := net.InterfaceAddrs()
 	routes = make(map[string]string)
-	checkErr(err)
 
-	ip := strings.Split(addresses[0].String(), "/")[0]
+	checkErr(err)
+	ip := addresses[1].String()
+	if len(addresses) == 3 {
+		ip = strings.Split(addresses[0].String(), "/")[0]
+	} else {
+		ip = addresses[1].String()[0 : len(ip)-3]
+	}
+
 	filename = flag.String("f", "invalid", "File name to be shared")
 	numberOfTimes = flag.Int("t", 1, "Number of times file to be shared")
 
@@ -37,14 +43,14 @@ func main() {
 		os.Exit(0)
 	}
 
-	file, err := os.Open("./" + *filename)
+	file, err := os.Open(*filename)
 	checkErr(err)
 	fileStat, err := file.Stat()
 	checkErr(err)
 
 	if fileStat.IsDir() {
-		zipname := *filename+".zip"
-		zipit("./" + *filename, zipname)
+		zipname := *filename + ".zip"
+		zipit(*filename, zipname)
 		routes["1"] = zipname
 	} else {
 		routes["1"] = *filename
@@ -66,7 +72,7 @@ func ServeRouterHandler(w http.ResponseWriter, r *http.Request) {
 func ServeFileHandler(w http.ResponseWriter, r *http.Request) {
 	file := r.URL.Path[len("/share/"):]
 	downloaded += 1
-	http.ServeFile(w, r, "./"+file)
+	http.ServeFile(w, r, file)
 	if downloaded >= *numberOfTimes {
 		log.Println("Downloads done, exiting")
 		os.Exit(0)
@@ -78,54 +84,55 @@ func checkErr(err error) {
 		log.Println("Error: ", err)
 	}
 }
+
 //zipit creates a "target".zip file from the source directory
 func zipit(source, target string) {
 	zipfile, err := os.Create(target)
 	checkErr(err)
 	defer zipfile.Close()
-	
+
 	archive := zip.NewWriter(zipfile)
 	defer archive.Close()
-	
+
 	info, err := os.Stat(source)
 	checkErr(err)
 	var baseDir string
-	
-	if info.IsDir(){
+
+	if info.IsDir() {
 		baseDir = filepath.Base(source)
 	}
-	
+
 	filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
 		header, err := zip.FileInfoHeader(info)
 		checkErr(err)
-		
+
 		if baseDir != "" {
 			header.Name = filepath.Join(baseDir, strings.TrimPrefix(path, source))
 		}
-		
+
 		if info.IsDir() {
 			header.Name += "/"
 		} else {
 			header.Method = zip.Deflate
 		}
-		
+
 		writer, err := archive.CreateHeader(header)
 		checkErr(err)
-		
+
 		if info.IsDir() {
 			return nil
 		}
-		
+
 		file, err := os.Open(path)
 		defer file.Close()
-		
+
 		_, err = io.Copy(writer, file)
-		
-		if err != nil{
+
+		if err != nil {
 			return nil
 		}
-		
+
 		return err
-		
+
 	})
 }
